aa47288365eead11adf95978c165e9a61d2ddc78
Sean Corbett <stcorbett@gmail.com>
2017-07-13 14:34:11 -0500

    ensure app runs in mHUB's time zone

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module SlackEvents
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.time_zone = ENV.fetch("EVENT_TIME_ZONE") { 'Central Time (US & Canada)' }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
